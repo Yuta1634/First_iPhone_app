536c113118c567da0234a02ad829ba9db9bed79c
Yuta1634 <yuta1994year@gmail.com>
2020-09-01 19:16:08 +0900

    Initial commit

---
+++ First_iPhone_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First_iPhone_app
+//
+//  Created by 高橋祐太 on 2020/08/22.
+//  Copyright © 2020 高橋祐太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
